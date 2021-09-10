e85b5879e5d40148dd5ff56018b71184ba080388
admin <admin@imidev>
2021-09-10 09:54:44 +0200

    added sender to addMessage

--- addMessage.php
+++ addMessage.php
@@ -2,11 +2,12 @@
 
 include 'utils.php';
 
-$uid = $_GET['UID'];
+$uid1 = $_GET['ORG'];
+$uid2 = $_GET['UID'];
 $msg = $_GET['MSG'];
 $now = time();
 
-$data = "\n>>>---\n$now\n$msg";
-file_put_contents("$MSGDIR/$uid", $data, FILE_APPEND | LOCK_EX);
+$data = "\n>>>---\n$now\n$uid1\n$msg";
+file_put_contents("$MSGDIR/$uid2", $data, FILE_APPEND | LOCK_EX);
 
 ?>
