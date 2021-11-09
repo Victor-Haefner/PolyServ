67c1f0c2af197cb6721206ccae4a82511b0b4dbc
Victor Haefner <victor.haefner@gmail.com>
2021-11-09 22:02:15 +0100

    added timeouts

--- test2.py
+++ test2.py
@@ -4,27 +4,15 @@ import socket, sys, os
 import threading
 from time import sleep
 
-def log(msg, doAppend = True):
-	if doAppend: f = open('logs/pylog.txt', 'a')
-	else: f = open('logs/pylog.txt', 'w')
-	f.write(msg+'\n')
-	f.close()
+def log(msg):
+	print msg
 
-sessionFile = sys.argv[1]
 serverIP = '0.0.0.0'
 
-log('py, start session '+sessionFile, False)
+log('py, start session')
 
-try:
-	f = open('sessions/'+sessionFile, 'r')
-except:
-	log('  session file "'+'sessions/'+sessionFile+'" could not be opened!')
-	exit(0)
-
-data = f.readlines()
-f.close()
-port1 = data[2][:-1]
-port2 = data[3][:-1]
+port1 = '7771'
+port2 = '7772'
 
 log(' client 1 on: '+port1)
 log(' client 2 on: '+port2)
@@ -34,11 +22,11 @@ port2 = int(port2)
 
 connectionMap = {}
 
-acceptTimeout = 5*60 # 5 min
-recvTimeout = 5*60 # 5 min
+acceptTimeout = 15.0
+recvTimeout = 15.0
 
 def startConnection(port, port2):
-	log(' start socket on: '+serverIP+':'+str(port))
+	log(' start socket on: '+serverIP+':'+str(port)+', timeout after '+str(acceptTimeout)+' s')
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 	sock.settimeout(acceptTimeout)
@@ -47,7 +35,7 @@ def startConnection(port, port2):
 	except:
 		log('  bind socket on '+str(port)+' failed!')
 	sock.listen(1)
-	log('  start listening')
+	log('  start listening, timeout after '+str(recvTimeout)+' s')
 
 	try:
 		connection, client_address = sock.accept()
@@ -56,13 +44,10 @@ def startConnection(port, port2):
 
 		try:
 			while True: # Receive the data in small chunks and retransmit it
+				print 'recv', port
 				connection.settimeout(recvTimeout)
 				data = connection.recv(256)
-				if data: 
-					log(' got "'+data+'" on port: '+str(port))
-					if port2 in connectionMap: 
-						log('  send "'+data+'" to port: '+str(port2))
-						connectionMap[port2].sendall(data)
+				if data: log(' got "'+data+'" on port: '+str(port))
 				else: break
             
             	except:
@@ -75,15 +60,27 @@ def startConnection(port, port2):
 	finally:
 		sock.close()
 
+def testSend(port, msg):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+	sock.connect(('localhost',port))
+	sock.send(msg)
+	return sock
+	#sock.close()
+
 t1 = threading.Thread(target=startConnection, args=(port1, port2,))
 t2 = threading.Thread(target=startConnection, args=(port2, port1,))
-
 t1.start()
 t2.start()
+
+sleep(2)
+client1 = testSend(port1, 'heloo '+str(port1))
+client2 = testSend(port2, 'heloo '+str(port2))
+
+
 t1.join()
 t2.join()
 log('  done')
-os.remove('sessions/'+sessionFile)
 
 
 	
