653d4cd4915b3ca788f0f137226eba842b0bc4e6
admin <admin@imidev>
2022-02-01 20:43:10 +0100

    fixing connections

--- getConnection.php
+++ getConnection.php
@@ -30,19 +30,23 @@ file_put_contents("$LOGDIR/log.getConnection.txt", "check for session $uid2-$uid
 if (file_exists("$SESDIR/$uid2-$uid")) { // case 2
 	$sessionFile = "$uid2-$uid";
 	$data = file("$SESDIR/$sessionFile");
-	$uri1 = rtrim($data[2]);
-	$uri2 = rtrim($data[3]);
+	$port2 = rtrim($data[2]);
+	$port1 = rtrim($data[3]);
+        $port4 = rtrim($data[5]);
+        $port3 = rtrim($data[6]);
 	sleep(1); // give python some time to start, see below
-	print "$uri2:case2:$uri1";
-	file_put_contents("$LOGDIR/log.getConnection.txt", "\n found existing session: $uid2-$uid, send ports: $uri2/$uri1", FILE_APPEND);
+	print "$port1:case2:$port2:$port3:$port4";
+	file_put_contents("$LOGDIR/log.getConnection.txt", "\n found existing session: $uid2-$uid, send ports: $port1/$port2 and $port3/$port4", FILE_APPEND);
 } else if (file_exists("$SESDIR/$uid-$uid2")) {
 	$sessionFile = "$uid-$uid2";
         $data = file("$SESDIR/$sessionFile");
-        $uri1 = rtrim($data[3]);
-        $uri2 = rtrim($data[2]);
+        $port2 = rtrim($data[3]);
+        $port1 = rtrim($data[2]);
+        $port4 = rtrim($data[6]);
+        $port3 = rtrim($data[5]);
         sleep(1); // give python some time to start, see below
-	print "$uri2:case2:$uri1";
-        file_put_contents("$LOGDIR/log.getConnection.txt", "\n found existing session: $uid-$uid2, send ports: $uri2/$uri1", FILE_APPEND);
+	print "$port1:case2:$port2:$port3:$port4";
+        file_put_contents("$LOGDIR/log.getConnection.txt", "\n found existing session: $uid-$uid2, send ports: $port1/$port2 and $port3/$port4", FILE_APPEND);
 } else { // case 1
 	// get free ports
 	$port1 = findPort(4000, 4100);
